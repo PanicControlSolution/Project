4f6d165de94a730f56d41131031d999302c85b3c
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-03-29 22:01:26 +0300

    Added interfaces and some methods

---
+++ Lab5.Application/Abstractions/ISushiService.cs
@@ -0,0 +1,9 @@
+﻿using Lab5.Domain.Entities;
+
+namespace Lab5.Application.Abstractions
+{
+    public interface ISushiService : IBaseService<Sushi>
+    {
+        public Task<IEnumerable<Sushi>> GetAllSet(int id);
+    }
+}
