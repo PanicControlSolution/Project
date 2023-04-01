502497fc8350def3cafe43865802969bd2e69128
VadVergasov <vadim.vergasov2003@gmail.com>
2023-04-01 22:19:17 +0300

    Specified type because we have Application namespace

--- Lab5.UI/App.xaml.cs
+++ Lab5.UI/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿namespace Lab5.UI {
-    public partial class App : Application {
-        public App() {
+﻿namespace Lab5.UI
+{
+    public partial class App : Microsoft.Maui.Controls.Application
+    {
+        public App()
+        {
             InitializeComponent();
 
             MainPage = new AppShell();
