d3b3d3bf82ce15c2c36f51b67cf53d15b080060b
VadVergasov <vadim.vergasov2003@gmail.com>
2023-04-01 22:20:12 +0300

    Using Maui in builder

--- Lab5.UI/MauiProgram.cs
+++ Lab5.UI/MauiProgram.cs
@@ -1,15 +1,21 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
-namespace Lab5.UI {
-    public static class MauiProgram {
-        public static MauiApp CreateMauiApp() {
+namespace Lab5.UI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                .ConfigureFonts(fonts => {
+                .ConfigureFonts(fonts =>
+                {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
+                .UseMauiCommunityToolkit();
 
 #if DEBUG
             builder.Logging.AddDebug();
