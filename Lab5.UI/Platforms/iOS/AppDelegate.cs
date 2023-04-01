4d7d65f7ea628ccf4d761bfa07759549b4c60a45
VadVergasov <vadim.vergasov2003@gmail.com>
2023-04-01 22:17:51 +0300

    Removed Tizen and iOS code

--- Lab5.UI/Platforms/iOS/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace Lab5.UI.Platforms.iOS {
-    [Register("AppDelegate")]
-    public class AppDelegate : MauiUIApplicationDelegate {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    }
-}
