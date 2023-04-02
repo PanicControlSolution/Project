5f42f8041c4830fac9341a8429ce5b3c78203bf5
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-04-02 22:31:31 +0300

    Completed User Story 11 (#5)
    - Changed visibility of some classes
    - Fixed some style issues
    - Removed Tizen and iOS platforms
    - Changed target Windows version to higher SDK version
    - Added services to MauiProgram

--- Lab5.UI/MauiProgram.cs
+++ Lab5.UI/MauiProgram.cs
@@ -1,21 +1,39 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Lab5.Application.Abstractions;
+using Lab5.Application.Services;
+using Lab5.Domain.Abstractions;
+using Lab5.Persistence.UnitOfWork;
+using Microsoft.Extensions.Logging;
 
-namespace Lab5.UI {
-    public static class MauiProgram {
-        public static MauiApp CreateMauiApp() {
+namespace Lab5.UI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                .ConfigureFonts(fonts => {
+                .ConfigureFonts(fonts =>
+                {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
+                .UseMauiCommunityToolkit();
 
 #if DEBUG
             builder.Logging.AddDebug();
-#endif
+#endif  
+            SetupServices(builder.Services);
 
             return builder.Build();
+        }
+
+        private static void SetupServices(IServiceCollection services)
+        {
+            services.AddSingleton<IUnitOfWork, FakeUnitOfWork>();
+            services.AddSingleton<ISetService, SetService>();
+            services.AddSingleton<ISushiService, SushiService>();
         }
     }
 }
