0f1efde6dbab451663bbd4836773114ba3b3968f
Pashutkaa <shchirovpavel@gmail.com>
2023-04-05 16:48:02 +0300

    UserStory 12(6 in scroma) done.

--- Lab5.UI/Pages/Sets.xaml.cs
+++ Lab5.UI/Pages/Sets.xaml.cs
@@ -1,9 +1,12 @@
+using Lab5.UI.ViewModels;
+
 namespace Lab5.UI.Pages;
 
 public partial class Sets : ContentPage
 {
-	public Sets()
+	public Sets(SetViewModel setViewModel)
 	{
 		InitializeComponent();
+        BindingContext = setViewModel;
 	}
 }
---
+++ Lab5.UI/ViewModels/SetViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Lab5.Application.Abstractions;
+using Lab5.Domain.Entities;
+using System.Collections.ObjectModel;
+
+namespace Lab5.UI.ViewModels
+{
+    public partial class SetViewModel : ObservableObject
+    {
+        private readonly ISetService _setService;
+        private readonly ISushiService _sushiService;
+        public SetViewModel(ISetService courceService, ISushiService
+        traineeService)
+        {
+            _setService = courceService;
+            _sushiService = traineeService;
+        }
+        public ObservableCollection<Set> Cources { get; set; } = new();
+        public ObservableCollection<Sushi> Trainees { get; set; } = new();
+
+        [ObservableProperty]
+        Set selectedSet;
+        [RelayCommand]
+        async void UpdateGroupList() => await GetCources();
+        [RelayCommand]
+        async void UpdateMembersList() => await GetTrainees();
+        public async Task GetCources()
+        {
+            var cources = await _setService.GetAllAsync();
+            await MainThread.InvokeOnMainThreadAsync(() =>
+            {
+                Cources.Clear();
+                foreach (var cource in cources)
+                    Cources.Add(cource);
+            });
+        }
+        public async Task GetTrainees()
+        {
+            var trainees = await _setService.GetAllBySetIdAsync(SelectedSet.Id);
+            await MainThread.InvokeOnMainThreadAsync(() =>
+            {
+                
+            Trainees.Clear();
+                foreach (var trainee in trainees)
+                    Trainees.Add(trainee);
+            });
+        }
+    }
+}
