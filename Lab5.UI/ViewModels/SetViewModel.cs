0f073abc249f0a680625bb3af508d20e5fe949d2
VadVergasov <vadim.vergasov2003@gmail.com>
2023-04-10 10:13:14 +0300

    Revert "Removed unused elements in ViewModel"
    This reverts commit 327e02b78266b8ba2abf33baca6ea74962c9e6f5.

--- Lab5.UI/ViewModels/SetViewModel.cs
+++ Lab5.UI/ViewModels/SetViewModel.cs
@@ -9,19 +9,24 @@ namespace Lab5.UI.ViewModels
     public partial class SetViewModel : ObservableObject
     {
         private readonly ISetService _setService;
+        private readonly ISushiService _sushiService;
 
-        public SetViewModel(ISetService setService)
+        public SetViewModel(ISetService setService, ISushiService sushiService)
         {
             _setService = setService;
+            _sushiService = sushiService;
         }
 
         public ObservableCollection<Set> Sets { get; set; } = new();
+        public ObservableCollection<Sushi> Sushi{ get; set; } = new();
 
         [ObservableProperty]
         Set selectedSet;
 
         [RelayCommand]
         async void UpdateGroupList() => await GetSets();
+        [RelayCommand]
+        async void UpdateMembersList() => await GetSushi();
 
         public async Task GetSets()
         {
@@ -32,6 +37,20 @@ namespace Lab5.UI.ViewModels
                 foreach (var cource in sets)
                     Sets.Add(cource);
             });
+        }
+
+        public async Task GetSushi()
+        {
+            var sushi = await _setService.GetAllBySetIdAsync(SelectedSet.Id);
+            await MainThread.InvokeOnMainThreadAsync(() =>
+            {
+
+                Sushi.Clear();
+                foreach (var trainee in sushi)
+                    Sushi.Add(trainee);
+            }
+            
+            );
         }
     }
 }
