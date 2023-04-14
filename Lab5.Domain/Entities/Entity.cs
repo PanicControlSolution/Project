f0f55b072a3147d769f0c45e61c5015504f3dfd1
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-04-14 21:11:53 +0300

    Done User Story 18 (#19)
    Added Photo property to entites

--- Lab5.Domain/Entities/Entity.cs
+++ Lab5.Domain/Entities/Entity.cs
@@ -4,5 +4,6 @@
     {
         public int Id { get; init; }
         public string Name { get; set; } = "";
+        public string Photo { get; set; }
     }
 }
--- Lab5.Domain/Entities/Set.cs
+++ Lab5.Domain/Entities/Set.cs
@@ -8,13 +8,14 @@
 
         public List<Sushi> Sushi { get; set; } = new();
 
-        public Set(int id, double cost, string name, string? description, double weight)
+        public Set(int id, double cost, string name, string? description, double weight, string photo = "empty.png")
         {
             Id = id;
             Cost = cost;
             Description = description;
             Weight = weight;
             Name = name;
+            Photo = photo;
         }
     }
 }
