33dfaa9ec25458e99d8e0a3aba79f34809b137cd
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-04-14 18:55:21 +0300

    Added Photo property to entites

--- Lab5.Domain/Entities/Sushi.cs
+++ Lab5.Domain/Entities/Sushi.cs
@@ -6,12 +6,13 @@
 
         public List<Set> Sets { get; set; }
 
-        public Sushi(int id, string name, int count, IEnumerable<Set> sets)
+        public Sushi(int id, string name, int count, IEnumerable<Set> sets, string photo = "empty.png")
         {
             Id = id;
             Count = count;
             Name = name;
             Sets = sets.ToList();
+            Photo = photo;
         }
     }
 }
