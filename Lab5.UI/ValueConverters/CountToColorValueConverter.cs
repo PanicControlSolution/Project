49461772a2547c10fc6ca2b919375903a2276447
Alexander <alexskvr03@gmail.com>
2023-04-17 00:41:33 +0300

    converter is ready

--- Lab5.UI/ValueConverters/CountToColorValueConverter.cs
+++ Lab5.UI/ValueConverters/CountToColorValueConverter.cs
@@ -13,11 +13,11 @@ namespace Lab5.UI.ValueConverters
         public Color Denied { get; set; } = Colors.Red;
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var sushi = (string)value;
-            int count = Int32.Parse(Regex.Match(sushi, @"\d+").Value);
-            if (count < 1) 
+            var count = (int)value;
+            if (count > 8)
                 return Denied;
             return Accept;
+
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
