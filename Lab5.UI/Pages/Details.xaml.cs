e296be93c4e9968aa07534390b295b5ceb5e717c
Alexander <alexskvr03@gmail.com>
2023-04-19 10:58:38 +0300

    fix of fix for @VadVergasov

--- Lab5.UI/Pages/Details.xaml.cs
+++ Lab5.UI/Pages/Details.xaml.cs
@@ -4,7 +4,9 @@ namespace Lab5.UI.Pages;
 
 public partial class Details : ContentPage
 {
+
     private DetailsViewModel _viewModel;
+    
     public Details(DetailsViewModel detailsViewModel)
     {
         InitializeComponent();
--- Lab5.UI/ViewModels/DetailsViewModel.cs
+++ Lab5.UI/ViewModels/DetailsViewModel.cs
@@ -7,8 +7,11 @@ namespace Lab5.UI.ViewModels
 {
     public partial class DetailsViewModel : IQueryAttributable, INotifyPropertyChanged
     {
+
         public event PropertyChangedEventHandler PropertyChanged;
+        
         Sushi selectedObject;
+        
         public Sushi SelectedObject
         {
             get => selectedObject;
