716247568c2292f47afc58abb584e651125cd3c9
Pashutkaa <shchirovpavel@gmail.com>
2023-04-28 22:09:31 +0300

    Fully done visual of AddSushiPage, ViewModel is not connected

--- Lab5.UI/Pages/AddSushiPage.xaml.cs
+++ Lab5.UI/Pages/AddSushiPage.xaml.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Lab5.UI.ViewModels;
 
 namespace Lab5.UI.Pages;
@@ -6,10 +8,25 @@ public partial class AddSushiPage : ContentPage
 {
     private SetViewModel _viewModel;
 
+
     public AddSushiPage(SetViewModel setViewModel)
     {
         InitializeComponent();
         _viewModel = setViewModel;
         BindingContext = _viewModel;
     }
+
+    public async void pick_image(object sender, EventArgs e)
+    {
+        var customFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+        {
+            {DevicePlatform.WinUI, new[]{".png", ".svg"} }
+        });
+           
+        var results = await FilePicker.PickAsync(new PickOptions
+        {
+            FileTypes = customFileType
+        });
+    }
+    
 }
